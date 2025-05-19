99fd0143d1411be9423b8131a5e0a213e2c5212c
Isaac <isaacujay@mail.com>
2025-05-19 22:16:56 +0100

    Portfolio-one week project initialcommit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,21 @@
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Hero/Hero";
+import About from "./components/About/About";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <About />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
