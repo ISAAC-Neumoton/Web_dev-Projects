4f704fed4bbabdc8d8dae4de904829300f4a0b95
Isaac <isaacujay@mail.com>
2025-05-20 19:04:15 +0100

    Hero section designed

--- my-react-app/src/components/Hero/Hero.jsx
+++ my-react-app/src/components/Hero/Hero.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import "./Hero.css"
+import myImage from "../../assets/developer.jpg"
 const Hero = () => {
   return (
-    <div>
-      
+    <div className='hero'>
+        <div className="hero-content">
+          <div className="image-container">
+            <img src= {myImage} alt="" className="image" />
+          </div>
+            <div className="hero-message">
+              <h1 className='name'>Ujay</h1>
+              <h2>Developer, Computer Scientist</h2>
+            </div>
+        
+        </div>
     </div>
   )
 }
